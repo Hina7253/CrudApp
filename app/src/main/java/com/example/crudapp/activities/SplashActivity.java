0da13e9d1e5387a456fd841f3e90b23a0ab892db
Hina7253 <hinakumari.725394@gmail.com>
2026-05-08 14:15:38 +0530

    update everything

---
+++ app/src/main/java/com/example/crudapp/activities/SplashActivity.java
@@ -0,0 +1,34 @@
+package com.example.crudapp.activities;
+
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.crudapp.R;
+import com.example.crudapp.utils.ValidationUtils;
+import com.example.crudapp.utils.SessionManager;
+
+public class SplashActivity extends AppCompatActivity {
+
+    private SessionManager sessionManager;
+    private static final int SPLASH_DELAY = 2000; // 2 seconds
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        sessionManager = new SessionManager(this);
+
+        new Handler().postDelayed(() -> {
+            if (sessionManager.isLoggedIn()) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+            } else {
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+            }
+            finish();
+        }, SPLASH_DELAY);
+    }
+}
