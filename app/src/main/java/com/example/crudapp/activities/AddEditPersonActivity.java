08cebf141f5cc994ccf358b86cae3ce64f5e4cfb
Hina7253 <hinakumari.725394@gmail.com>
2026-05-09 13:34:35 +0530

    update

--- app/src/main/java/com/example/crudapp/activities/AddEditPersonActivity.java
+++ app/src/main/java/com/example/crudapp/activities/AddEditPersonActivity.java
@@ -3,7 +3,7 @@ package com.example.crudapp.activities;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
+
 import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
